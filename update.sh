5e728fbb1468b898e8a6ea7b1d522178c56ae326
Pilo11 <Nils.Buhmann95@gmail.com>
2018-12-27 15:32:53 +0100

    Modified

--- update.sh
+++ update.sh
@@ -1,8 +1,10 @@
-e#!/bin/zsh
+#!/bin/zsh
 # This script will install or update the current baremetal OS
+# parameters:
+# 1. username of the main user (NOT ROOT)
 
 # Save current user as variable
-CURRUSER="$USER"
+CURRUSER="$1"
 
 # check if script executed with root rights
 if [[ $EUID -ne 0 ]]; then
@@ -12,17 +14,20 @@ fi
 
 # change to home directroy
 echo "Change working dir to home directory of the current user..."
-cd ~
+cd /home/$CURRUSER
 
 # 1. Update all pacman updates without asking for anything
-pacman -Syyu
+echo "Update all installed packages"
+pacman -Syyu --needed --noconfirm > /dev/null
 # 2. install base-devel
-pacman -Sy base-devel
+echo "Install base-devel if needed"
+pacman -Sy base-devel --needed --noconfirm > /dev/null
 # 3. change to build directory (if not exist, create one)
 mkdir -p build
 cd build
 # 4. clone yay repo and install it
-git clone https://aur.archlinux.org/yay.git
+echo "Install yay if needed"
+sudo -u $CURRUSER git clone https://aur.archlinux.org/yay.git
 cd yay
 sudo -u $CURRUSER makepkg -sri --skippgpcheck --noconfirm
 cd ..
