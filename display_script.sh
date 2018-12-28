b396834e0c448259088ecd265031552471d1f8b9
Pilo11 <Nils.Buhmann95@gmail.com>
2018-12-28 15:14:46 +0100

    Added profile system + implemented nvidia config stuff + need testing

---
+++ display_script.sh
@@ -0,0 +1,3 @@
+xrandr --setprovideroutputsource modesetting NVIDIA-0
+xrandr --auto
+xrandr --output HDMI-0 --above eDP-1-1
--- update.sh
+++ update.sh
@@ -2,9 +2,12 @@
 # This script will install or update the current baremetal OS
 # parameters:
 # 1. username of the main user (NOT ROOT)
+# 2. profile of installing (LAPTOP, DESKTOP, SERVER)
 
 # Save current user as variable
 CURRUSER="$1"
+PROFILE="$2"
+CURRPATH="$PWD"
 
 # check parameter
 if [[ -z "$1" ]]; then
@@ -20,42 +23,160 @@ else
   fi
 fi
 
+if [[ -z "$2" ]]; then
+  echo "You did not choose any profile, so it will only do the standard installation"
+fi
+
 # check if script executed with root rights
 if [[ $EUID -ne 0 ]]; then
   echo "This script must be run as root" 1>&2
   exit 1
 fi
 
-# change to home directroy
-echo "Change working dir to home directory of the current user..."
+# methods and functions
+
+COUNTER=0
+
+# Increment counter
+function inc {
+  COUNTER=$((COUNTER + 1))
+}
+
+# This method updates all pacman packages without asking
+function update {
+  pacman -Syyu --needed --noconfirm > /dev/null
+}
+
+# This method updates all AUR packages without asking
+function yupdate {
+  yay -Syyu --needed --noconfirm > /dev/null
+}
+
+# This method checks if it is necessary to install a package and do it if needed.
+function install {
+  pacman -Sy $1 --needed --noconfirm > /dev/null
+}
+
+# This method checks if it is necessary to install an AUR package and do it if needed.
+function yinstall {
+  yay -Sy $1 --needed --noconfirm > /dev/null
+}
+
+# This method checks if a package is installed
+function check {
+  pacman -Qi $1 > /dev/null
+}
+
+# This methods checks the init things like yay, base-devel etc and install and configure it if needed
+function init {
+  # Update all pacman updates without asking for anything
+  inc
+  echo "$COUNTER. Update all installed packages"
+  update
+  # Install base-devel
+  inc
+  echo "$COUNTER. Install base-devel if needed"
+  install "base-devel"
+  # Change to build directory (if not exist, create one)
+  inc
+  echo "$COUNTER. Change to build directory"
+  mkdir -p build
+  cd build
+  # Clone yay repo and install it
+  inc
+  echo "$COUNTER. Install yay if needed"
+  # Check if yay is installed
+  check yay
+  YAY_CHECK=$?
+  if [ $YAY_CHECK -ne 0 ]; then
+    sudo -u $CURRUSER git clone https://aur.archlinux.org/yay.git > /dev/null
+    cd yay
+    sudo -u $CURRUSER makepkg -sri --skippgpcheck --noconfirm > /dev/null
+    cd ..
+    rm -rf yay
+  else
+    echo "YAY already exist"
+  fi
+  # Install oh my zsh
+  inc
+  echo "$COUNTER. Install oh-my-zsh"
+  # Check if it is installed already
+  if [ -d "/home/$CURRUSER/.oh-my-zsh" ]; then
+    echo "oh my zsh already found."
+  else
+    # Clone the git repo
+    sudo -u $CURRUSER git clone https://github.com/robbyrussell/oh-my-zsh.git /home/$CURRUSER/.oh-my-zsh > /dev/null
+    # Backup original zsh file
+    sudo -u $CURRUSER cp /home/$CURRUSER/.zshrc /home/$CURRUSER/.zshrc.orig > /dev/null
+    # Copy new zsh config
+    sudo -u $CURRUSER cp /home/$CURRUSER/.oh-my-zsh/templates/zshrc.zsh-template /home/$CURRUSER/.zshrc > /dev/null
+    # Set ZSH as new standard console
+    chsh -s /bin/zsh > /dev/null
+  fi
+  # Patch makeconf file to prevent COMPRESSING AUR packages when installing
+  inc
+  echo "$COUNTER. Patch makepkg.conf for preventing COMPRESSION of AUR packages"
+  patch /etc/makepkg.conf < $CURRPATH/patches/makepkg.conf.patch
+}
+
+# Blacklist nouveau and install nvidia driver
+function nvidia {
+  if [ -f /etc/modprobe.d/blacklist.conf ]; then
+    TEMP_DATA=$( cat /etc/modprobe.d/blacklist.conf | grep nouveau )
+    if [ -n $TEMP_DATA ]; then
+      echo "Nouveau already blacklisted"
+    else
+      # Blacklist nouveau driver
+      echo "Blacklist nouveau driver"
+      touch /etc/modprobe.d/blacklist.conf
+      patch /etc/modprobe.d/blacklist.conf < $CURRPATH/patches/blacklist.conf.patch
+      # Install nvidia and nvidia utils
+      echo "Install nvidia and nvidia utils"
+      yinstall nvidia
+      yinstall nvidia-utils
+      # Copy display script
+      echo "Copy display script"
+      cp $CURRPATH/display_script.sh /home/$CURRUSER/display_script.sh
+      echo "Copy nvidia X11 configuration"
+      cp $CURRPATH/20-intel.conf /etc/X11/xorg.conf.d/20-intel.conf
+      echo "Apply patch for lightdm config"
+      patch /etc/lightdm/lightdm.conf < $CURRPATH/patches/lightdm.conf.patch
+    fi
+  fi
+}
+
+# ---------------------------
+# ----- code execution ------
+# ---------------------------
+
+# change to home directory
+inc
+echo "$COUNTER. Change working dir to home directory of the current user..."
 cd /home/$CURRUSER
 
-# 1. Update all pacman updates without asking for anything
-echo "1. Update all installed packages"
-pacman -Syyu --needed --noconfirm > /dev/null
-# 2. install base-devel
-echo "2. Install base-devel if needed"
-pacman -Sy base-devel --needed --noconfirm > /dev/null
-# 3. change to build directory (if not exist, create one)
-echo "3. Change to build directory"
-mkdir -p build
-cd build
-# 4. clone yay repo and install it
-echo "4. Install yay if needed"
-# Check if yay is installed
-pacman -Qi yay > /dev/null
-YAY_CHECK=$?
-if [ $YAY_CHECK -ne 0 ]; then
-  sudo -u $CURRUSER git clone https://aur.archlinux.org/yay.git > /dev/null
-  cd yay
-  sudo -u $CURRUSER makepkg -sri --skippgpcheck --noconfirm > /dev/null
-  cd ..
-  rm -rf yay
-else
-  echo "YAY already exist"
-fi
-# TODO: install oh-my-zsh
-# TODO: remove packaging from pacman.conf
-# TODO: remove nouveau and install nvidia + X11 conf + lightdm conf
-# TODO: install gnome-terminals + feh for i3 config
-# TODO: add i3 config + status bar + images + xrandr settings
+# These have to be called for all profiles
+init
+
+inc
+# The following have to be called only if needed for the specific profiles
+case "$PROFILE" in
+'LAPTOP')
+  echo "$COUNTER. Do profile work: $PROFILE"
+  inc
+  echo "$COUNTER. Install nvidia stuff and blacklist nouveau"
+  nvidia
+  ;;
+'DESKTOP')
+  echo "$COUNTER. Do profile work: $PROFILE"
+  ;;
+'SERVER')
+  echo "$COUNTER. Do profile work: $PROFILE"
+  ;;
+*)
+  echo "No profile was chosen, so nothing to do..."
+  ;;
+esac
+
+# TODO: add i3 config + status bar + images + xrandr settings (gnome-terminals + feh)
+# TODO: vmware install + preferences (LIKE Keyboard shortcuts)
+# TODO: check multigesture availability and create multigesture shortcut mapping (3 finger tap)
